60a46253c98110c83ca9ce633edc4e48b0a98e18
xmyhero1995x <62993845+xmyhero1995x@users.noreply.github.com>
2022-02-12 22:15:01 +0300

    Add files via upload
    fixed/added sliders

--- script.js
+++ script.js
@@ -2,52 +2,24 @@
 
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
-            $('.header__burger,.header__menu,.header__burger-menu,.header__shopping-bag-mobile').toggleClass('active');
-            $('body').toggleClass('lock');
-        });
+        $('.header__burger,.header__menu,.header__burger-menu,.header__shopping-bag-mobile').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
-
-// Слайдер для team.css
-
-const teamSlider = document.querySelector('.team__slider-line');
-
-document.querySelector('.team__arrow-right').addEventListener('click', function() {
-    offset = offset + 197;
-    if (offset > 591) {
-        offset = 0;
-    }
-    teamSlider.style.left = -offset + 'px';
-});
-
-// Слайдер для reviews.css
-
-let offset = 0;
-const reviewsSlider = document.querySelector('.reviews__slider-line');
-
-// Вправо
-document.querySelector('.reviews__arrow-left').addEventListener('click', function() {
-    offset = offset + 197;
-    if (offset > 591) {
-        offset = 0;
-    }
-    reviewsSlider.style.left = -offset + 'px';
-});
-
-
-// Влево
-document.querySelector('.reviews__arrow-right').addEventListener('click', function() {
-    offset = offset - 197;
-    if (offset < 0) {
-        offset = 591;
-    }
-    reviewsSlider.style.left = -offset + 'px';
+// Slider
+const swiper = new Swiper(".slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
 
 // Слайдер для products.css
 // Вправо
+let offset = 0;
 const productsSlider = document.querySelector('.products__slider-line');
 
-document.querySelector('.products__arrow-right').addEventListener('click', function() {
+document.querySelector('.products__arrow-right').addEventListener('click', function () {
     offset = offset + 285;
     if (offset > 855) {
         offset = 0;
@@ -56,7 +28,7 @@ document.querySelector('.products__arrow-right').addEventListener('click', funct
 });
 
 // Влево
-document.querySelector('.products__arrow-left').addEventListener('click', function() {
+document.querySelector('.products__arrow-left').addEventListener('click', function () {
     offset = offset - 285;
     if (offset < 0) {
         offset = 855;
